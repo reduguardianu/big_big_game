d0368258a95c40e4cf62e4c7fff77d12d7542203
miko <milosz.kordecki@gmail.com>
2016-07-18 20:27:31 +0200

    papaj

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Player : MonoBehaviour {
 
@@ -10,23 +12,73 @@ public class Player : MonoBehaviour {
 
 	StageConfig stage;
 
+
+	Dictionary<SpeedMod, float> speedMods;
+
+
+	float moddedAcc = 0;
+	float moddedMaxSpeed = 0;
+
+
+	float elapsed = 0;
+
+
 	bool initialized = false;
 
 	public void Init(StageConfig s) {
+		speedMods = new Dictionary<SpeedMod, float>();
 		stage = s;
 
 		initialized = true;
 	}
 
+	void ProccessSpeedMods() {
+		moddedMaxSpeed = stage.maxSpeed;
+		moddedAcc = stage.acceleration;
+
+		var sorted = speedMods.Keys.OrderBy(x => speedMods[x]);
+
+		foreach (SpeedMod speedMod in sorted) {
+			if (speedMods[speedMod] + speedMod.duration < elapsed) {
+				continue;
+			}
+
+			if (speedMod.accMul > 0) {
+				moddedAcc += moddedAcc * speedMod.accMul;
+			}
+			moddedAcc += speedMod.accAdd;
+
+			if (speedMod.maxSpeedMul > 0) {
+				moddedMaxSpeed += moddedMaxSpeed * speedMod.maxSpeedMul;
+			}
+			moddedMaxSpeed += speedMod.maxSpeedAdd;
+
+		}
+	}
+
+	void ApplySpeedMod(SpeedMod mod) {
+		speedMods[mod] = elapsed;
+		ProccessSpeedMods();
+		currentSpeed += mod.oneTimeSpeed;
+	}
+
+
+
 	void Update() {
 		if (!initialized) {
 			return;
 		}
-		currentSpeed = Mathf.Clamp(currentSpeed + stage.acceleration * Time.deltaTime, 0, stage.maxSpeed);
+
+		elapsed += Time.deltaTime;
+
+		ProccessSpeedMods();
+
+		currentSpeed = Mathf.Clamp(currentSpeed + moddedAcc * Time.deltaTime, 0, moddedMaxSpeed);
 		distance += currentSpeed * Time.deltaTime;
 
 		gameObject.transform.position = new Vector3(distance, 0, 0);
 	}
+
 
 
 }
---
+++ Assets/Resources/Scripts/SpeedMod.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedMod : MonoBehaviour {
+
+	public float duration;
+	public float oneTimeSpeed;
+
+	public float maxSpeedAdd;
+	public float maxSpeedMul;
+
+	public float accAdd;
+	public float accMul;
+
+}
