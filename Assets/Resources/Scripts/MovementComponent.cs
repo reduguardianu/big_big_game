a28089ade8688ece73d84daaddaa5171487374c3
Ajronfyst <ajronfyst>
2016-07-20 20:15:40 +0200

    Mateusz mockup particle i gotowa animacja

--- Assets/Resources/Scripts/MovementComponent.cs
+++ Assets/Resources/Scripts/MovementComponent.cs
@@ -12,22 +12,35 @@ public class MovementComponent : MonoBehaviour {
     [SerializeField]
     string dash;
 
-	void Start () {
+    [SerializeField]
+    GameObject particles;
+
+    void Start () {
     }
 
 	void Update () {
 
         
         this.GetComponent<Player>().playerAnimation.SetBool("jump", !GetComponent<Player>().isOnGround);
+
         if (Input.GetButtonDown(jump) && this.GetComponent<Player>().isOnGround) 
         {
             jumpPressTime = Time.time;
             Debug.Log("click jump");
         }
-        if (Input.GetButtonDown(dash) && Time.time > dashUseTime + 0.5f) {
+        if (Input.GetButtonDown(dash) && Time.time > dashUseTime + 0.5f)
+        {
             this.GetComponent<Rigidbody>().AddForce(new Vector3(60, 0, 0));
             dashUseTime = Time.time;
+            this.GetComponent<Player>().playerAnimation.SetTrigger("dash");
+            particles.SetActive(true);
         }
+
+        if (Time.time - dashUseTime > 0.4f)
+        {
+            particles.SetActive(false);
+        }
+
 
         if (Input.GetButton(jump) && Time.time - jumpPressTime <= 0.2f)
         {
