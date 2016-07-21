f12cb71c553f1aa8f75623bcae925695dd694e48
Ajronfyst <ajronfyst>
2016-07-21 22:51:17 +0200

    chuiSplash jesli uwazacie ze mozemy tego uzyc to spoko :D

---
+++ Assets/Video.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Video : MonoBehaviour
+{
+    public MovieTexture mt;
+    public GameObject hideBg;
+    float timeToEnd;
+	// Use this for initialization
+	void Start ()
+    {
+        hideBg.SetActive(false);
+        mt.Play();
+        timeToEnd = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Time.time - timeToEnd >= (mt.duration - 0.1f))
+        {
+            Debug.Log("HIDE");
+            hideBg.SetActive(true);
+        }
+       /* if (!mt.isPlaying)
+        {
+            hideBg.SetActive(true);
+
+        }*/
+    }
+}
