5464cf0c03702705e24ed14bcc647e70d3cc2ddb
Ajronfyst <ajronfyst>
2016-07-27 02:09:09 +0200

    chui_splash

--- Assets/Video.cs
+++ Assets/Video.cs
@@ -5,6 +5,9 @@ public class Video : MonoBehaviour
 {
     public MovieTexture mt;
     public GameObject hideBg;
+    public GameObject particle1;
+    public GameObject particle2;
+
     float timeToEnd;
 	// Use this for initialization
 	void Start ()
@@ -13,10 +16,18 @@ public class Video : MonoBehaviour
         mt.Play();
         timeToEnd = Time.time;
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
+        if (Time.time - timeToEnd >= 0.25f && !particle1.activeSelf)
+        {
+            particle1.SetActive(true);
+        }
+        if (Time.time - timeToEnd >= 1.30f && !particle2.activeSelf)
+        { 
+            particle2.SetActive(true);
+        }
         if (Time.time - timeToEnd >= (mt.duration - 0.1f))
         {
             Debug.Log("HIDE");
