93bdb9649886e987fe6bda31c07beca3ad9344dc
Ajronfyst <ajronfyst>
2016-07-28 04:09:59 +0200

    tla

---
+++ Assets/VideoKeynes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoKeynes : MonoBehaviour {
+    public MovieTexture mt;
+
+    // Use this for initialization
+    void Start () {
+        mt.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        mt.Play();
+
+    }
+}
